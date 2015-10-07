fd07a96e97930d8f840f48893a4822e18adbe640
Justin Brown <jbrown23@gmail.com>
2015-10-06 18:49:58 -0700

    Allow subtrees by id.

--- js/family_tree.js
+++ js/family_tree.js
@@ -1,4 +1,6 @@
 function buildTree(json) {
+    var param = getParameterByName('id');
+    json = findRoot(param, json);
 
   var tree = d3.layout.tree()
     .separation(function() {
@@ -23,6 +25,7 @@ function buildTree(json) {
     .append("g");
 
   var nodes = tree.nodes(json);
+  // console.log(nodes);
   var link = svg.selectAll(".link")
     .data(tree.links(nodes))
     .enter().append("path")
@@ -109,4 +112,51 @@ var printDeath = function(d) {
 
 function elbow(d, i) {
   return "M" + d.source.y + "," + d.source.x + "H" + d.target.y + "V" + d.target.x + (d.target.children ? "" : "h" + 120);
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function dec2bin(str){
+    var dec = parseInt(str, 10);
+    return (dec >>> 0).toString(2);
+}
+
+function parentExists(json, i) {
+    if (typeof json === 'undefined') return false;
+    if (typeof json.parents === 'undefined') return false;
+    return (typeof json.parents[i] !== 'undefined');
+}
+
+function findRoot(param, root) {
+    if (param === "" || param === "0") return root;
+    var s = dec2bin(param);
+    if (isNaN(s) || s.length < 2) return root;
+
+    var json = root;
+    for (var i = 1; i < s.length; i++) {
+	if (s.charAt(i) == '0' && parentExists(json, 0) && json.parents[0].male) {
+	    json = json.parents[0];
+	    continue;
+	}
+	if (s.charAt(i) == '0' && parentExists(json, 1) && json.parents[1].male) {
+	    json = json.parents[1];
+	    continue;
+	}
+	if (s.charAt(i) == '1' && parentExists(json, 0) && !json.parents[0].male) {
+	    json = json.parents[0];
+	    continue;
+	}
+	if (s.charAt(i) == '1' && parentExists(json, 1) && !json.parents[1].male) {
+	    json = json.parents[1];
+	    continue;
+	}
+	// cannont continue so I should bail;
+	return root;
+    }
+    return json;
 }
